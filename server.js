8a53a7a4b5ae50ef327ffb1249c0d40a3b9bcd85
Allan Mageto <allanbmageto@gmail.com>
2020-05-03 16:37:00 +0300

    added list all images

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
+const port = process.env.PORT || 8889
+const directoryPath = process.cwd() + "/images";
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -37,21 +39,20 @@ var upload = multer({
   },
 });
 
-app.listen(8889, () => {
-  console.log("Started on port 8889");
-  const dir = process.cwd() + "/images";
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir, (error) => {
+app.listen(port, () => {
+  if (!fs.existsSync(directoryPath)) {
+    fs.mkdirSync(directoryPath, (error) => {
       if (error) {
         console.error(error);
       } else {
+        console.log("Started on port" +  port);
         console.log("Images directory has been created");
       }
     });
   }
 });
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("Its you not me ! :D");
 });
 
 app.post("/image", (req, res) => {
@@ -70,4 +71,20 @@ app.post("/images", upload.array("images"), (req, res) => {
     console.log(error);
     res.send(400);
   }
+});
+
+app.get("/listAllImages", (req, res) => {
+  fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+      res.status(500).send(err)
+      return console.log("Unable to scan directory: " + err);
+    }
+    //listing all files using forEach
+    var filesArray = [];
+    files.forEach(function (file) {
+      filesArray.push(file);
+    });
+    res.send(filesArray);
+  });
 });
